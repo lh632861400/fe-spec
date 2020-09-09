45b768bce37323f2b8b7bbb2ada6040d15909dfd
svgkankan <lhadzxczxc@126.com>
2020-09-09 14:00:22 +0800

    doc: 修改菜单和路由设置
    为了支持自定义路由

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,21 @@
 
+const menus = [
+  {
+    title: '开始使用',
+    children: [
+      // 菜单子项（可选）
+      'index.md', // 对应的 Markdown 文件，路径是相对于 resolve.includes 目录识别的
+    ],
+  },
+  {
+    title: 'html规范',
+    children: [
+      // 菜单子项（可选）
+      'html/code-spec.md', // 对应的 Markdown 文件，路径是相对于 resolve.includes 目录识别的
+    ],
+  },
+];
+
 export default {
   base: '/',
   description: 'aa',
@@ -18,23 +35,12 @@ export default {
       path: 'https://github.com/lh632861400/fe-spec',
     },
   ],
-  menus: {
-    // 需要自定义侧边菜单的路径，没有配置的路径还是会使用自动生成的配置
-    "/": [
-      {
-        title: '概览',
-        children: [
-          // 菜单子项（可选）
-          'index.md', // 对应的 Markdown 文件，路径是相对于 resolve.includes 目录识别的
-        ],
-      },
-      {
-        title: 'html规范',
-        children: [
-          // 菜单子项（可选）
-          'html/code-spec.md', // 对应的 Markdown 文件，路径是相对于 resolve.includes 目录识别的
-        ],
-      },
-    ],
-  },
+  routes: [
+    {
+      path: '/',
+      routes: [
+
+      ],
+    },
+  ],
 }
